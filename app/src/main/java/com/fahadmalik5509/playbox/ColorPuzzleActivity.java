ed017eef243534c1f45e5be1414576194346d852
fahadmalik5509 <fahadmalik5509@gmail.com>
2025-02-16 00:14:26 +0500

    v1

--- app/src/main/java/com/fahadmalik5509/playbox/ColorPuzzleActivity.java
+++ app/src/main/java/com/fahadmalik5509/playbox/ColorPuzzleActivity.java
@@ -21,18 +21,30 @@ import java.util.Random;
 public class ColorPuzzleActivity extends AppCompatActivity {
 
     private ColorpuzzleLayoutBinding vb;
-    private byte MAX_GRID_SIZE = 9, currentGridSize = 3, numberOfLives = 3, successCount = 0, consecutiveWin = 0;
-    private int colorDelta = 30, score = 0;
-    private boolean newLevel = false;
+    private static final byte INITIAL_GRID_SIZE = 3;
+    private static final byte MAX_GRID_SIZE = 9;
+    private static final byte MAX_LIVES = 3;
+    private static final byte INITIAL_COLOR_DELTA = 30;
+    private static final byte LOWEST_COLOR_DELTA = 5;
+    private static final byte CHANGE_IN_COLOR_DELTA = 3;
+
+    private byte currentGridSize = INITIAL_GRID_SIZE,  currentColorDelta = INITIAL_COLOR_DELTA, numberOfLives = MAX_LIVES, successCount = 0, consecutiveWin = 0;
+    private int currentScore = 0;
+    private boolean isGridChange = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         vb = ColorpuzzleLayoutBinding.inflate(getLayoutInflater());
         setContentView(vb.getRoot());
-        updateBestScoreDisplay();
-        vb.gridContainer.setLayoutTransition(new LayoutTransition());
+
+        setupGame();
+    }
+
+    private void setupGame() {
         animateViewPulse(this, vb.resetTV, true);
+        vb.gridContainer.setLayoutTransition(new LayoutTransition());
+        updateBestScoreDisplay();
         generateGrid(currentGridSize);
     }
 
@@ -82,8 +94,8 @@ public class ColorPuzzleActivity extends AppCompatActivity {
             } else {
                 handleLoss(button);
             }
-            playSound(this, isTarget ? (newLevel ? R.raw.newlevel : R.raw.sucess) : R.raw.aheartbreak);
-            newLevel = false;
+            playSound(this, isTarget ? (isGridChange ? R.raw.newlevel : R.raw.sucess) : R.raw.aheartbreak);
+            isGridChange = false;
         });
         return button;
     }
@@ -94,14 +106,16 @@ public class ColorPuzzleActivity extends AppCompatActivity {
         handleLevelUp();
         handleLifeIncrement();
 
-        score++;
+        currentScore++;
         updateScoreDisplay();
-        if (score > sharedPreferences.getInt(PUZZLE_BEST_SCORE, 0)) {
-            saveToSharedPreferences(PUZZLE_BEST_SCORE, score);
+
+        if (currentScore > sharedPreferences.getInt(PUZZLE_BEST_SCORE, 0)) {
+            saveToSharedPreferences(PUZZLE_BEST_SCORE, currentScore);
             updateBestScoreDisplay();
             vb.fireLAV.playAnimation();
             vb.fireLAV.setVisibility(VISIBLE);
         }
+
         generateGrid(currentGridSize);
     }
 
@@ -111,14 +125,14 @@ public class ColorPuzzleActivity extends AppCompatActivity {
         vb.fireLAV.setVisibility(GONE);
         consecutiveWin = 0;
 
-        if (numberOfLives > 0) {
-            numberOfLives--;
-        }
+        if (numberOfLives > 0) numberOfLives--;
+
         if (numberOfLives == 0) {
-            toggleVisibility(true, vb.shadowV);
+            toggleVisibility(true, vb.shadowV, vb.gameOverLAV);
             vb.gameOverLAV.playAnimation();
             playSound(this, R.raw.agameover);
         }
+
         playHeartAnimation();
     }
 
@@ -126,17 +140,16 @@ public class ColorPuzzleActivity extends AppCompatActivity {
         if (successCount >= 5) {
             if (currentGridSize < MAX_GRID_SIZE) {
                 currentGridSize++;
-                newLevel = true;
+                isGridChange = true;
             }
-            if (colorDelta > 5) {
-                colorDelta -= 5;
+            if (currentColorDelta > LOWEST_COLOR_DELTA) {
+                currentColorDelta -= CHANGE_IN_COLOR_DELTA;
             }
             successCount = 0;
         }
     }
-
     private void handleLifeIncrement() {
-        if (consecutiveWin >= 5) {
+        if (consecutiveWin == 5 || (consecutiveWin == 3 && currentScore >=25)) {
             if (numberOfLives < 3) {
                 numberOfLives++;
             }
@@ -144,24 +157,30 @@ public class ColorPuzzleActivity extends AppCompatActivity {
             resetHearts();
         }
     }
-
     private void resetHearts() {
         switch (numberOfLives) {
             case 3:
+                vb.heartOneLAV.cancelAnimation();
+                vb.heartTwoLAV.cancelAnimation();
+                vb.heartThreeLAV.cancelAnimation();
+
                 vb.heartOneLAV.setFrame(0);
                 vb.heartTwoLAV.setFrame(0);
                 vb.heartThreeLAV.setFrame(0);
                 break;
             case 2:
+                vb.heartOneLAV.cancelAnimation();
+                vb.heartTwoLAV.cancelAnimation();
+
                 vb.heartOneLAV.setFrame(0);
                 vb.heartTwoLAV.setFrame(0);
                 break;
             case 1:
+                vb.heartTwoLAV.cancelAnimation();
                 vb.heartTwoLAV.setFrame(0);
                 break;
         }
     }
-
     private void playHeartAnimation() {
         switch (numberOfLives) {
             case 2:
@@ -175,64 +194,52 @@ public class ColorPuzzleActivity extends AppCompatActivity {
                 break;
         }
     }
-
-    private void updateScoreDisplay() {
-        vb.currentScoreTV.setText("Score: " + score);
-    }
-
+    private void updateScoreDisplay() { vb.currentScoreTV.setText("Score: " + currentScore); }
     private void updateBestScoreDisplay() {
         int bestScore = sharedPreferences.getInt(PUZZLE_BEST_SCORE, 0);
         vb.bestScoreTV.setText("Best Score: " + bestScore);
     }
-
     public void handleResetClick(View view) {
         playSound(this, R.raw.click_ui);
-        score = 0;
-        updateScoreDisplay();
-        numberOfLives = 3;
-        currentGridSize = 3;
-        consecutiveWin = 0;
-        successCount = 0;
-        colorDelta = 30;
-        toggleVisibility(false, vb.shadowV);
-        vb.gameOverLAV.setFrame(0);
-        vb.heartOneLAV.setFrame(0);
-        vb.heartTwoLAV.setFrame(0);
-        vb.heartThreeLAV.setFrame(0);
-        vb.gameOverLAV.cancelAnimation();
-        vb.fireLAV.cancelAnimation();
-        vb.fireLAV.setVisibility(GONE);
+        resetGameState();
         generateGrid(currentGridSize);
     }
-
+    private void resetGameState() {
+        currentScore = 0;
+        consecutiveWin = 0;
+        successCount = 0;
+        numberOfLives = MAX_LIVES;
+        currentGridSize = INITIAL_GRID_SIZE;
+        currentColorDelta = INITIAL_COLOR_DELTA;
+        toggleVisibility(false, vb.shadowV, vb.fireLAV, vb.gameOverLAV);
+        resetHearts();
+        updateScoreDisplay();
+    }
     private int getBaseColor() {
         return Color.rgb(getRandomNumber(0, 256), getRandomNumber(0, 256), getRandomNumber(0, 256));
     }
-
     private int getTargetColor(int baseColor) {
-        int adjust = new Random().nextBoolean() ? colorDelta : -colorDelta;
-        int r = Math.min(255, Math.max(0, Color.red(baseColor) + adjust));
-        int g = Math.min(255, Math.max(0, Color.green(baseColor) + adjust));
-        int b = Math.min(255, Math.max(0, Color.blue(baseColor) + adjust));
-        return Color.rgb(r, g, b);
+        int adjust = new Random().nextBoolean() ? currentColorDelta : -currentColorDelta;
+        return Color.rgb(
+                clampColorValue(Color.red(baseColor) + adjust),
+                clampColorValue(Color.green(baseColor) + adjust),
+                clampColorValue(Color.blue(baseColor) + adjust)
+        );
     }
-
+    private int clampColorValue(int value) { return Math.min(255, Math.max(0, value)); }
     private int dpToPx(int dp) {
         return Math.round(dp * getResources().getDisplayMetrics().density);
     }
-
     public void goToSetting(View view) {
         playSound(this, R.raw.click_ui);
         Intent intent = new Intent(this, SettingActivity.class);
         intent.putExtra("origin_activity", getClass().getSimpleName());
         startActivity(intent);
     }
-
     public void goToHome(View view) {
         playSound(this, R.raw.click_ui);
         changeActivity(this, HomeActivity.class, true, false);
     }
-
     public void goBack(View view) {
         playSound(this, R.raw.click_ui);
         changeActivity(this, GameModeActivity.class, true, false);
